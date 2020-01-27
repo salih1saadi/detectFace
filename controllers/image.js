baac2c938a1d36cdabaed60c8a2dd49f412192df
salih saadi <salih-sadi@hotmail.com>
2020-01-27 03:46:25 +0200

    adding server

---
+++ controllers/image.js
@@ -0,0 +1,39 @@
+
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '70a09e5088604ee9b6fd9e710d0a33d3'
+});
+
+const handleApiCall = (req,res) =>{
+
+app.models
+.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+.then(data => {
+	res.json(data);
+})
+.catch(err => res.status(400).json('unable to work with api'))
+
+}
+
+
+
+const handleImage = (req, res, db) =>{
+
+const { id } = req.body;
+
+db('users').where('id','=', id)
+.increment('entries', 1)
+.returning('entries')
+.then(entries => {
+
+	res.json(entries[0]);
+
+})
+.catch(err => res.status(400).json('unable to get entries'))
+
+}
+module.exports = {
+	handleImage,
+	handleApiCall
+};
