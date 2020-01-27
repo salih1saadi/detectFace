98ead836ed499ec3e7986d4f08434734b0d24337
salih saadi <salih-sadi@hotmail.com>
2020-01-27 05:55:46 +0200

    DS

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
  const db = knex ({
   client: 'pg',
   connection: {
-    host : 'postgresql-cubic-97567',
-    user : '',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl:true
   }
 });
 
