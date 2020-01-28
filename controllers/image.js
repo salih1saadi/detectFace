e894f8feddb4db161670c7c42bf5ca241e6e4842
salih saadi <salih-sadi@hotmail.com>
2020-01-28 05:22:00 +0200

    fsdaf

--- controllers/image.js
+++ controllers/image.js
@@ -16,16 +16,7 @@ app.models
 
 }
 
-//const recognizeApiCall = (req,res) =>{
 
-//app.models
-//.predict(Clarifai.CELEBRITY_MODEL, req.body.input)
-//.then(data => {
-//	res.json(data);
-//})
-//.catch(err => res.status(400).json('unable to work with api'))
-
-//}
 
 
 
