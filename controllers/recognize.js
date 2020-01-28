c52ba089837eb9a7fbc16520efe158b7ddd32035
salih saadi <salih-sadi@hotmail.com>
2020-01-28 04:08:36 +0200

    safasff

--- controllers/recognize.js
+++ controllers/recognize.js
@@ -1,30 +1,20 @@
-
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
  apiKey: '70a09e5088604ee9b6fd9e710d0a33d3'
 });
 
-const handleApiCall = (req,res) =>{
-
-app.models
-.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-.then(data => {
-	res.json(data);
-})
-.catch(err => res.status(400).json('unable to work with api'))
-
-}
 const recognizeApiCall = (req,res) =>{
 
 app.models
-.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+.predict(Clarifai.CELEBRITY_MODEL, req.body.input)
 .then(data => {
 	res.json(data);
 })
 .catch(err => res.status(400).json('unable to work with api'))
 
 }
+
 
 
 
@@ -45,6 +35,5 @@ db('users').where('id','=', id)
 }
 module.exports = {
 	handleImage,
-	handleApiCall,
 	recognizeApiCall
 };
--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.post('/signin', (req, res)=>{signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register',(req,res) => {register.handleRegister(req, res, db, bcrypt)})
 
 app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req, res, db)})
-
 app.put('/image',(req,res) => {image.handleImage(req, res, db)})
+app.put('/recognize',(req,res) => {recognize.recognizeApiCall(req, res, db)})
 app.post('/imageUrl',(req,res) => {image.handleApiCall(req, res)})
 
 
